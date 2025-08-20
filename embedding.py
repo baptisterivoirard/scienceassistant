87db1723258eab597562ff94fc3c7ce8280f2438
Baptiste Rivoirard <rivoirardbaptiste2@gmail.com>
2025-08-20 14:58:50 +0200

    script embedding biobert

---
+++ embedding.py
@@ -0,0 +1,46 @@
+import torch 
+from transformers import AutoModel , AutoTokenizer
+
+import numpy as np
+import faiss
+
+abstracts = [
+    "Breast cancers with HER2 amplification have a higher risk of CNS metastasis.",
+    "TLR4 is a key receptor in innate immune response against LPS.",
+    "Xylocaine is a local anesthetic drug widely used in surgery."
+]
+
+question = "What receptors are involved in immune response to LPS?"
+
+def embedding (chunks, question):
+    model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1", torch_dtype="auto")
+    tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    
+    inputs = tokenizer(chunks, return_tensors="pt", padding =True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+        # Moyenne des tokens
+    embedings = outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
+    
+    embedings = np.array(embedings).astype(np.float32)
+
+    faiss.normalize_L2(embedings)
+    dimension = embedings.shape[1]
+    index = faiss.IndexFlatIP(dimension)
+    index.add(embedings)
+
+    question_tokenized = tokenizer(question, return_tensors="pt", padding = True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs_q = model(**question_tokenized)
+        
+    question_embedding = outputs_q.last_hidden_state.mean(dim=1).squeeze().numpy()
+    question_embedding = np.array(question_embedding).astype(np.float32)
+    question_embedding = question_embedding.reshape(1, -1)
+    faiss.normalize_L2(question_embedding)
+
+    distances , indices =index.search(question_embedding, k=1)
+    return indices, distances
+
+
+if __name__== "__main__":
+    print(embedding(abstracts, question))
