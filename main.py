59456bec49eee841c303f22de8400d65d6d2aacd
Baptiste Rivoirard <rivoirardbaptiste2@gmail.com>
2025-08-29 08:31:37 +0200

    complete pipeline

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import subprocess 
-
+from NCBIquery import search_abstract, retrieve_abstracts
+from preprocessing import preprocessing
+from chunking import chunker
+from embedding import embedding
 
 
 
@@ -7,8 +10,25 @@ import subprocess
 
 def main():
     question = input("Enter your scientific question :")
+    nb_abstract = input("What is the depth of the search ? (number of abstracts to retrieve (max 20))")
     query = subprocess.run (["ollama", "run", "llama_keyword:latest"], input= question, text=True, capture_output=True)
-    return(query.stdout)
+    pmids = search_abstract(query.stdout, nb_abstract)
+    abstracts = retrieve_abstracts(pmids)
+    processed_abstracts = preprocessing(abstracts)
+    chunks = chunker(processed_abstracts, 500, 50)
+    indices = embedding(chunks, question, 3)
+    important_chunks = []
+    for k in indices[0]:
+        print (chunks[k])
+        important_chunks.append(chunks[k])
+    prompt_complement = '\n'.join(important_chunks)
+    prompt = f"Using the following context : {prompt_complement} \n Answer the following question : {question}"
+    reponse = subprocess.run(["ollama", "run", "llama_sientist:latest"], input=prompt, text=True, capture_output=True)
+
+    
+
+
+    return reponse.stdout
 
 
 
